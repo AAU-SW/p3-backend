2c38e59b5696fab515379792b201a4ef974bc92e
Kevin Nielsen <kevinanielsen@outlook.com>
2025-10-12 13:18:35 +0200

    initialize spring boot project

---
+++ src/test/java/aau/sw/p3/P3ApplicationTests.java
@@ -0,0 +1,13 @@
+package aau.sw.p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
