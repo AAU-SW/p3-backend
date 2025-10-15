bb22634d916de8dad9235b2cb1adfd159f252a5b
Oliver Acs <oliver_acs@yahoo.com>
2025-10-15 13:42:01 +0200

    added import deletemapping

--- src/main/java/aau/sw/controller/CaseController.java
+++ src/main/java/aau/sw/controller/CaseController.java
@@ -3,6 +3,7 @@ package aau.sw.controller;
 import aau.sw.model.Case;
 import aau.sw.repository.CaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
