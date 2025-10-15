8e2f28f2b0dcce9cd1e7dacf3aefd0046069b46c
Oliver Acs <oliver_acs@yahoo.com>
2025-10-15 14:20:40 +0200

    added delete method for users

--- src/main/java/aau/sw/controller/UserController.java
+++ src/main/java/aau/sw/controller/UserController.java
@@ -1,27 +1,41 @@
 package aau.sw.controller;
+
 import aau.sw.model.User;
 import aau.sw.service.UserService;
 import aau.sw.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-    
+
     @Autowired
     private UserRepository userRepository;
 
     private final UserService svc;
-    public UserController(UserService svc) { this.svc = svc; }
+
+    public UserController(UserService svc) {
+        this.svc = svc;
+    }
 
     @PostMapping
-    public User createUser(@RequestBody User user){
+    public User createUser(@RequestBody User user) {
         return userRepository.save(user);
 
     }
-  
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        if (!userRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
     @PutMapping("/{id}")
     public void updateUser(@PathVariable String id, @RequestBody String name) {
         svc.updateUser(id, name);
@@ -29,5 +43,5 @@ public class UserController {
             user.setName(name);
             userRepository.save(user);
         });
-  }
+    }
 }
