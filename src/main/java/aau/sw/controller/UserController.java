75309947559a705487c67a7b324e7d3ff0f58f82
tobiasandersen98 <karl.tobias.andersen@gmail.com>
2025-10-15 13:05:04 +0200

    whit put and post

--- src/main/java/aau/sw/controller/UserController.java
+++ src/main/java/aau/sw/controller/UserController.java
@@ -1,11 +1,33 @@
 package aau.sw.controller;
 
+import aau.sw.model.Case;
+import aau.sw.model.User;
+import aau.sw.repository.UserRepository;
 import aau.sw.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
     private final UserService svc;
     public UserController(UserService svc) { this.svc = svc; }
+
+    @PostMapping
+    public User createUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @PutMapping("/{id}")
+    public void updateUser(@PathVariable String id, @RequestBody String name) {
+        svc.updateUser(id, name);
+        userRepository.findById(id).ifPresent(user -> {
+            user.setName(name);
+            userRepository.save(user);
+        });
+}
 }
--- src/main/java/aau/sw/service/UserService.java
+++ src/main/java/aau/sw/service/UserService.java
@@ -7,4 +7,8 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository repo;
     public UserService(UserRepository repo) { this.repo = repo; }
+    public void updateUser(String id, String newName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
+    }
 }
