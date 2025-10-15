fa6165313188c0e58124d861a8c0b143a50ec53f
Kevin Nielsen <kevinanielsen@outlook.com>
2025-10-15 14:11:56 +0200

    fix build issues

--- src/main/java/aau/sw/controller/UserController.java
+++ src/main/java/aau/sw/controller/UserController.java
@@ -1,25 +1,20 @@
 package aau.sw.controller;
-import aau.sw.model.Case;
 import aau.sw.model.User;
-import aau.sw.repository.CaseRepository;
 import aau.sw.service.UserService;
 import aau.sw.repository.UserRepository;
 
-main
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-
+    
     @Autowired
     private UserRepository userRepository;
+
     private final UserService svc;
     public UserController(UserService svc) { this.svc = svc; }
-
-    @Autowired
-    private UserRepository userRepository;
 
     @PostMapping
     public User createUser(@RequestBody User user){
