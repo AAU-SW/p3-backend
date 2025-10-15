eda902485c50624b8ff19882d9eda0d0425595c8
Oliver Acs <oliver_acs@yahoo.com>
2025-10-15 13:11:58 +0200

    added delete case method

--- src/main/java/aau/sw/controller/CaseController.java
+++ src/main/java/aau/sw/controller/CaseController.java
@@ -3,11 +3,14 @@ package aau.sw.controller;
 import aau.sw.model.Case;
 import aau.sw.repository.CaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 @RestController
@@ -23,8 +26,18 @@ public class CaseController {
     }
 
     @GetMapping
-    public List<Case> getCases(){
+    public List<Case> getCases() {
         return caseRepository.findAll();
 
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCases(@PathVariable String id) {
+        if (!caseRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        caseRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
