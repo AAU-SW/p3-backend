eb78a2681dbb4f668f4ad7f439a28dc74bd3a502
Elias <100373290+EliasRazi@users.noreply.github.com>
2025-10-15 12:55:13 +0200

    Sw 14 - Read cases (#4)
    * initial case read
    
    * Users would like to read cases
    
    * Delete src/main/java/aau/sw/.DS_Store
    
    * Delete src/main/java/aau/.DS_Store
    
    * Delete .DS_Store
    
    * Delete src/main/.DS_Store
    
    * Delete src/main/java/.DS_Store
    
    * Delete src/.DS_Store
    
    * ds store added to ignore
    
    ---------
    
    Co-authored-by: Oliver Acs <oliver_acs@yahoo.com>
    Co-authored-by: Kevin Nielsen <kevinanielsen@outlook.com>

--- src/main/java/aau/sw/controller/CaseController.java
+++ src/main/java/aau/sw/controller/CaseController.java
@@ -3,10 +3,12 @@ package aau.sw.controller;
 import aau.sw.model.Case;
 import aau.sw.repository.CaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cases")
@@ -18,5 +20,11 @@ public class CaseController {
     @PostMapping
     public Case createCase(@RequestBody Case newCase) {
         return caseRepository.save(newCase);
+    }
+
+    @GetMapping
+    public List<Case> getCases(){
+        return caseRepository.findAll();
+
     }
 }
