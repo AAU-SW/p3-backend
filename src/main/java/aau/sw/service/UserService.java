8107644842ab737f9cd88194098d85bd3b0c546e
karl Tobias Nymann Andersen <74189898+TobiasAndersen98@users.noreply.github.com>
2025-10-15 13:53:49 +0200

    sw-15 As a user I would like the ability to update Cases (#8)
    * whit put and post
    
    * error for an emty name or id

--- src/main/java/aau/sw/service/UserService.java
+++ src/main/java/aau/sw/service/UserService.java
@@ -6,11 +6,24 @@ import aau.sw.repository.UserRepository;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
     private final UserRepository repo;
     public UserService(UserRepository repo) { this.repo = repo; }
+
+
+    @Transactional
+    public void updateUser(String id, String newName) {
+        if (newName == null || newName.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+        var user = repo.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("User not found" + id));
+            user.setName(newName);
+            repo.save(user);
+        };
 
     public List<User> all() {
         return repo.findAll();
@@ -18,4 +31,6 @@ public class UserService {
 
     public User getById(String id) { return repo.findById(id).orElse(null); }
     public User getByEmail(String email) { return repo.findByEmail(email); }
+
 }
+
