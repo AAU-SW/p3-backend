a34a276e7049d406f881ab862997345859fc358a
Mathildearntklov <arntklow@gmail.com>
2025-10-15 14:01:22 +0200

    delete asset (#12)
    Co-authored-by: Mathilde Arntkløv Stanley Petersen <mathildepetersen@MacBook-Air-tilhrende-Mathilde.local>

--- src/main/java/aau/sw/controller/AssetController.java
+++ src/main/java/aau/sw/controller/AssetController.java
@@ -1,16 +1,26 @@
 package aau.sw.controller;
-
 import aau.sw.model.Asset;
 import aau.sw.repository.AssetRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+
 
 @RestController
 @RequestMapping("/api/assets")
 public class AssetController {
     @Autowired
     private AssetRepository assetRepository;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAsset(@PathVariable String id) {
+        if (!assetRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        assetRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
 
     @PostMapping
     public Asset createAsset(@RequestBody Asset newAsset) {
