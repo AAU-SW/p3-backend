5f3810ac09e0cbe3b3d4593d2b83f98a6ad493b5
Oliver Acs <oliver_acs@yahoo.com>
2025-10-15 11:09:28 +0200

    initial case read

--- src/main/java/aau/sw/controller/CaseController.java
+++ src/main/java/aau/sw/controller/CaseController.java
@@ -3,6 +3,7 @@ package aau.sw.controller;
 import aau.sw.model.Case;
 import aau.sw.repository.CaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +19,8 @@ public class CaseController {
     @PostMapping
     public Case createCase(@RequestBody Case newCase) {
         return caseRepository.save(newCase);
+
+    @GetMapping
+
     }
 }
