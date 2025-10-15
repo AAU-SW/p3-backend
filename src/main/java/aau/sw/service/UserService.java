ded50e5e63e4b6604751464febd8280e791316b0
Kevin Nielsen <kevinanielsen@outlook.com>
2025-10-15 13:34:31 +0200

    add get all and getByEmail functionality for users (#7)

--- src/main/java/aau/sw/service/UserService.java
+++ src/main/java/aau/sw/service/UserService.java
@@ -1,10 +1,21 @@
 package aau.sw.service;
 
+import aau.sw.model.User;
 import aau.sw.repository.UserRepository;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository repo;
     public UserService(UserRepository repo) { this.repo = repo; }
+
+    public List<User> all() {
+        return repo.findAll();
+    }
+
+    public User getById(String id) { return repo.findById(id).orElse(null); }
+    public User getByEmail(String email) { return repo.findByEmail(email); }
 }
