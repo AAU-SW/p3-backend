8cd3848d2003cf4bcfdd973786e15d9f9772d3c8
Kevin Nielsen <kevinanielsen@outlook.com>
2025-10-15 11:06:52 +0200

    fix?

--- src/test/java/aau/sw/SWApplicationTests.java
+++ src/test/java/aau/sw/SWApplicationTests.java
@@ -7,6 +7,5 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @SpringBootTest
 class SWApplicationTests {
-    @Test
-    void contextLoads() { }
+    @Test void contextLoads() {}
 }
