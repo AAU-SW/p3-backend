42c6d80bf38277c855ba5236ddc04e357f77c636
Mathilde Arntkløv Stanley Petersen <mathildepetersen@MacBook-Air-tilhrende-Mathilde.local>
2025-10-15 14:20:18 +0200

    delete asset

--- src/main/java/aau/sw/controller/AssetController.java
+++ src/main/java/aau/sw/controller/AssetController.java
@@ -4,7 +4,6 @@ import aau.sw.repository.AssetRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 
 
@@ -14,16 +13,31 @@ public class AssetController {
     @Autowired
     private AssetRepository assetRepository;
   
+    // create asset
     @PostMapping
     public Asset createAsset(@RequestBody Asset newAsset) {
         return assetRepository.save(newAsset);
     }
-
+    
+    // read asset
     @GetMapping
     public List<Asset> getAssets(){
         return assetRepository.findAll();
     }
 
+    // update asset
+    @PutMapping("/{id}")
+    public void updateAsset(@PathVariable String id, @RequestBody Asset updatedAsset) {
+        assetRepository.findById(id).ifPresent(asset -> {
+            asset.setName(updatedAsset.getName());
+            asset.setDescription(updatedAsset.getDescription());
+            asset.setStatus(updatedAsset.getStatus());
+            asset.setRegistrationNumber(updatedAsset.getRegistrationNumber());
+            assetRepository.save(asset);
+        });
+    }
+
+    // delete asset
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAsset(@PathVariable String id) {
         if (!assetRepository.existsById(id)) {
