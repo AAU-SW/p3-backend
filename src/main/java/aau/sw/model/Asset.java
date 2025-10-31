b14d67fd3bf135f06a947e1d9a639cd8bc5ee591
karl Tobias Nymann Andersen <74189898+TobiasAndersen98@users.noreply.github.com>
2025-10-31 17:11:41 +0100

    sw-90: On creation of object(POST), the route should automatically add createdBy based on JWT token (#27)

--- src/main/java/aau/sw/model/Asset.java
+++ src/main/java/aau/sw/model/Asset.java
@@ -1,15 +1,15 @@
 package aau.sw.model;
 
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Document(collection = "assets")
-public class Asset {
+public class Asset implements Auditable {
 
     public enum Status {
         ACTIVE,
--- src/main/java/aau/sw/service/AssetService.java
+++ src/main/java/aau/sw/service/AssetService.java
@@ -1,10 +1,22 @@
 package aau.sw.service;
 
+
+import aau.sw.model.Asset;
 import aau.sw.repository.AssetRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AssetService {
-    private final AssetRepository repo;
-    public AssetService(AssetRepository repo) { this.repo = repo; }
+    private final AssetRepository assetRepository ;
+    private final AuditableService auditableService;
+
+    public AssetService(AssetRepository assetRepository, AuditableService auditableService) { 
+        this.assetRepository = assetRepository;
+        this.auditableService = auditableService;
+    }
+
+    public Asset createAsset(Asset asset){
+        auditableService.setCreatedBy(asset); // Set creator 
+        return assetRepository.save(asset);   // save to MongoDB
+    }
 }
