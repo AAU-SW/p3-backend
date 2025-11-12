65b522d519f79249b132b19e901d74361cbb9769
karl Tobias Nymann Andersen <karl.tobias.andersen@gmail.com>
2025-11-12 20:13:21 +0100

    Sw-146: add get contoler by id

--- src/main/java/aau/sw/controller/OrderController.java
+++ src/main/java/aau/sw/controller/OrderController.java
@@ -1,16 +1,22 @@
 package aau.sw.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import aau.sw.aspect.LogExecution;
 import aau.sw.model.Order;
 import aau.sw.repository.OrderRepository;
 import aau.sw.service.OrderService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
@@ -33,8 +39,19 @@ public class OrderController {
     }
 
     @GetMapping
+    @LogExecution("Fetched all Orders")
     public List<Order> getOrders() {
         return orderRepository.findAll();
 
     }
+
+    @GetMapping("/{id}")
+    @LogExecution("Fetched Order by ID: ")
+    public ResponseEntity<Order> getOrderById(@PathVariable String id) {
+        return orderRepository.findById(id)
+                .map(order -> ResponseEntity.ok().body(order))
+                .orElse(ResponseEntity.notFound().build());
+    }
+    
+    
 }
