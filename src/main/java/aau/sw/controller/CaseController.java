07b8af9cb9cf884d29802b5a8da64de37ce964ed
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-11-13 13:48:14 +0100

    fix import

--- src/main/java/aau/sw/controller/CaseController.java
+++ src/main/java/aau/sw/controller/CaseController.java
@@ -9,7 +9,6 @@ import aau.sw.service.AuditableService;
 import aau.sw.repository.ImageRepository;
 import aau.sw.service.CaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
