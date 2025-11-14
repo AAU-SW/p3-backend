1087f4399c8a8287457d5e5515d0782f67d8a1d2
Kevin Nielsen <kevinanielsen@outlook.com>
2025-11-14 12:33:19 +0100

    Add unit tests for order related functionality (#43)
    * add unit tests for OrderService
    
    * add unit tests for OrderController
    
    * make sure ci workflow runs tests as well
    
    ---------
    
    Co-authored-by: Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>

--- src/test/java/aau/sw/SWApplicationTests.java
+++ src/test/java/aau/sw/SWApplicationTests.java
@@ -3,9 +3,15 @@ package aau.sw;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+// No @MockBean usage; test beans provided by TestBeansConfig under test profile.
 
 @ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(classes = {SWApplication.class, aau.sw.config.TestBeansConfig.class, aau.sw.config.TestMongoConfig.class})
 class SWApplicationTests {
-    @Test void contextLoads() {}
+
+    // Provide minimal mocks so the application context can start without MongoDB or S3
+    // Bean mocks supplied by TestBeansConfig.
+
+    @Test
+    void contextLoads() {}
 }
