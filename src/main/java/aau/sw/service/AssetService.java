905608070837ae3b60e1d40a6dc7cc37dd42616a
tobiasandersen98 <karl.tobias.andersen@gmail.com>
2025-11-14 14:27:48 +0100

    sw-111: get all asset whet a order nr

--- src/main/java/aau/sw/service/AssetService.java
+++ src/main/java/aau/sw/service/AssetService.java
@@ -3,8 +3,11 @@ package aau.sw.service;
 
 import aau.sw.model.Asset;
 import aau.sw.model.Image;
+import aau.sw.model.Order;
 import aau.sw.repository.AssetRepository;
 import aau.sw.repository.ImageRepository;
+import aau.sw.repository.OrderRepository;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,21 +19,43 @@ public class AssetService {
     private final AuditableService auditableService;
     private final ImageRepository imageRepository;
     private final FileService fileService;
+    private final OrderRepository orderRepository;
 
-    public AssetService(AssetRepository assetRepository, AuditableService auditableService, ImageRepository imageRepository, FileService fileService) {
+    public AssetService(AssetRepository assetRepository, 
+                        AuditableService auditableService, 
+                        ImageRepository imageRepository, 
+                        FileService fileService,
+                        OrderRepository orderRepository) {
         this.assetRepository = assetRepository;
         this.auditableService = auditableService;
         this.imageRepository = imageRepository;
         this.fileService = fileService;
+        this.orderRepository = orderRepository;
     }
 
     public Asset createAsset(Asset asset){
         auditableService.setCreatedBy(asset); // Set creator 
+        if (asset.getOrderRef() != null && asset.getOrderRef().getId() != null) {
+            String orderId = asset.getOrderRef().getId();
+            Order order = orderRepository.findById(orderId)
+                    .orElseThrow(() -> new RuntimeException("Order not found with id: " + orderId));
+                
+            asset.setOrderRef(order);
+        }
+
         return assetRepository.save(asset);   // save to MongoDB
     }
 
     public Asset createAssetWithImage(Asset asset, MultipartFile imageFile) throws IOException {
         auditableService.setCreatedBy(asset);
+        if (asset.getOrderRef() != null && asset.getOrderRef().getId() != null) {
+            String orderId = asset.getOrderRef().getId();
+            Order order = orderRepository.findById(orderId)
+                    .orElseThrow(() -> new RuntimeException("Order not found with id: " + orderId));
+                
+            asset.setOrderRef(order);
+        }
+
 
         String fileExtension = getFileExtension(imageFile.getOriginalFilename());
 
