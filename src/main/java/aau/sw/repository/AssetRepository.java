997c67ff99e23bfbc47bf54d307fd63d31edbaf3
Mathias Storgaard <130549468+Mathavs0810@users.noreply.github.com>
2025-11-22 16:52:14 +0100

    fixed orderref

--- src/main/java/aau/sw/repository/AssetRepository.java
+++ src/main/java/aau/sw/repository/AssetRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AssetRepository extends MongoRepository<Asset, String> {
-    List<Asset> findByOrderRef_Id(String orderId); 
+    List<Asset> findByOrderRef_Id(String orderRef);
 }
