1949bf08d98f8a4e5be1dc6977ef326a69cb5806
Kevin Nielsen <kevinanielsen@outlook.com>
2025-12-18 17:49:34 +0100

    fix missing import

--- src/main/java/aau/sw/model/Asset.java
+++ src/main/java/aau/sw/model/Asset.java
@@ -6,8 +6,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.annotation.CreatedDate;
 
+import aau.sw.dto.AssetDto;
 
 
 @Document(collection = "assets")
